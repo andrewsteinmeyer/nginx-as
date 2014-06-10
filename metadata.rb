001ffcfcbe51ae052cb3701299d2ece4dfa51732
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2014-06-10 18:09:42 -0400

    adding first commit of nginx conf

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "nginx-as"
+maintainer        "Andrew Steinmeyer"
+maintainer_email  "jandrewsteinmeyer@gmail.com"
+description       "Installs the nginx web server from the nginx ppa"
+version           "0.0.1"
+
+recipe "nginx-tlq", "nginx server"
+
+supports "ubuntu"
